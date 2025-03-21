87f9189f372eed0dd88b2e1b8c585bed7c6c5eee
psravani580 <sravani.pasam99@gmail.com>
2025-03-21 19:45:57 +0100

    Refactored combined the conditions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -23,11 +23,7 @@ class GildedRose {
                 item.quality = item.quality + 1;
 
                 if (isBackstagePasses(item)) {
-                    if (item.sellIn < 11) {
-                        incrementQuality(item);
-                    }
-
-                    if (item.sellIn < 6) {
+                    if (item.sellIn < 11 || item.sellIn < 6) {
                         incrementQuality(item);
                     }
                 }
